a4704ea4e1915048c7011a6e0af6f9a69b09b0ee
Johnny-Tech <fatormajohn64@gmail.com>
2026-09-06 16:32:55 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+  APP_NAME: "The Golden Loaf Bakery",
+
+  EMAIL: "",
+  PHONE_NUMBER: "",
+  WHATSAPP_NUMBER: "",
+
+  FACEBOOK_URL: "",
+  INSTAGRAM_URL: "",
+  TIKTOK_URL: "",
+
+  ADDRESS: "",
+  OPENING_HOURS: "",
+
+  API_URL: ""
+};
